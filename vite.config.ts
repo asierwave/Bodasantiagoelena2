0ae9c3fcc184e81145c8bac4cc7b800bac5ff706
asierwave <125518587+asierwave@users.noreply.github.com>
2025-11-24 19:16:30 +0100

    Configura proyecto para gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+      base: '/Bodasantiagoelena2/', // <-- reemplaza REPO_NAME
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
